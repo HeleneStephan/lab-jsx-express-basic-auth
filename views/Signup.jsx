c46528b191aa92a1e25612bf0e22424db15d9eed
Helene Stephan <helene.stephan@protonmail.com>
2020-11-17 19:44:25 +0100

    td

--- views/Signup.jsx
+++ views/Signup.jsx
@@ -1,8 +1,26 @@
 const React = require("react");
 const Layout = require("./Layout");
 
-function Signup() {
-  return;
+function Signup(props) {
+  return (
+    <Layout>
+      <form id="form" action="/auth/signup" method="POST">
+        <label>Username</label>
+        <br />
+        <input type="text" name="username" placeholder="username" />
+
+        <label>Password</label>
+        <br />
+        <input type="password" name="password" placeholder="password" />
+
+        <button type="submit">Sign Up</button>
+
+        {props.errorMessage ? (
+          <div className="error-message"> {props.errorMessage} </div>
+        ) : null}
+      </form>
+    </Layout>
+  );
 }
 
 module.exports = Signup;
